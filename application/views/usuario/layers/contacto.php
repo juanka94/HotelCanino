3bf4b7b57937ad06d7fefde04a04073ff8833e1d
antonio0554 <anto.cast.gtz@gmail.com>
2016-04-04 14:58:49 -0500

    contacto

--- application/views/usuario/layers/contacto.php
+++ application/views/usuario/layers/contacto.php
@@ -2,8 +2,8 @@
       <div class="container login">
         <div class="row">
           <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-            <h2>Contact Us</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p>
+            <h2>Contactanos</h2>
+            <p>Tienes alguna duda o sugerencia con nosotros, mandanos un correo con tus datos y tu duda, y pronto la resolveremos.</p>
           </div>
         </div>
         <div class="contact-form wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
@@ -13,35 +13,36 @@
                 <div class="row  wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
                   <div class="col-sm-6">
                     <div class="form-group">
-                      <input type="text" name="name" class="form-control" placeholder="Name" required="required">
+                      <input type="text" name="name" class="form-control" placeholder="Nombre Completo" required="required">
                     </div>
                   </div>
                   <div class="col-sm-6">
                     <div class="form-group">
-                      <input type="email" name="email" class="form-control" placeholder="Email Address" required="required">
+                      <input type="email" name="email" class="form-control" placeholder="Correo Electronico" required="required">
                     </div>
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="text" name="subject" class="form-control" placeholder="Subject" required="required">
+                  <input type="text" name="subject" class="form-control" placeholder="Asunto" required="required">
                 </div>
                 <div class="form-group">
-                  <textarea name="message" id="message" class="form-control" rows="4" placeholder="Enter your message" required="required"></textarea>
+                  <textarea name="message" id="message" class="form-control" rows="4" placeholder="Escribe tus dudas o sugerencias" required="required"></textarea>
                 </div>                        
                 <div class="form-group">
-                  <button type="submit" class="btn-submit">Send Now</button>
+                  <button type="submit" class="btn-submit">Enviar Ahora</button>
                 </div>
               </form>   
             </div>
             <div class="col-sm-6">
               <div class="contact-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                 <ul class="address">
-                  <li><i class="fa fa-map-marker"></i> <span> Address:</span> 2400 South Avenue A </li>
-                  <li><i class="fa fa-phone"></i> <span> Phone:</span> +928 336 2000  </li>
-                  <li><i class="fa fa-envelope"></i> <span> Email:</span><a href="mailto:someone@yoursite.com"> support@oxygen.com</a></li>
-                  <li><i class="fa fa-globe"></i> <span> Website:</span> <a href="#">www.sitename.com</a></li>
+                  <li><i class="fa fa-map-marker"></i> <span> Direccion:</span> CALLE INDEPENDENCIA 53, Rancho de Villa, Colima Colima.</li>
+                  <li><i class="fa fa-phone"></i> <span> Telefono:</span> +52 312 3081600  </li>
+                  <li><i class="fa fa-phone"></i> <span> Celular:</span> +52 312 140 60 16  </li>
+                  <li><i class="fa fa-envelope"></i> <span> Email:</span><a href="mailto:someone@yoursite.com"> contacto@hotelcaninoreyes.com</a></li>
+                  <li><i class="fa fa-globe"></i> <span> Sitio Web:</span> <a href="#">www.hotelcaninoreyes.com</a></li>
                 </ul>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15069.020671333437!2d-103.769341!3d19.227708!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84254f843a7b252d%3A0xc158a5f8b03c0f06!2sIndependencia+199%2C+Ejidal%2C+Lo+de+Villa%2C+28620+Colima%2C+Col.%2C+M%C3%A9xico!5e0!3m2!1ses!2sus!4v1459799796368" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
               </div>                            
             </div>
           </div>
